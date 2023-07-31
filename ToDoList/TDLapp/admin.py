a2b4f0c6b597a89330bd5f5c38db2beb7df7d43b
sk1p3r <annanlusige@gmail.com>
2023-07-31 23:13:09 +0300

    finalizing

---
+++ ToDoList/TDLapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from . models import TDL,Item
+
+# Register your models here.
+
+admin.site.register(TDL)
+admin.site.register(Item)
---
+++ ToDoList/TDLapp/views.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpResponseRedirect
+from . models import TDL, Item
+from . forms import CreateNewList
+# Create your views here.
+
+def index(response, id):
+    ls = TDL.objects.get(id=id)
+
+    if response.method == "POST":
+        print(response.POST)
+        if response.POST.get("save"):
+            for item in ls.item_set.all():
+                if response.POST.get("c" + str(item.id)) == "clicked":
+                    item.complete = True
+                else:
+                    item.complete = False
+                item.save()
+        elif response.POST.get("newItem"):
+            txt = response.POST.get("new")
+            if len(txt)>2:
+                ls.item_set.create(text=txt, complete=False)
+            else:
+                print("Invalid")
+    return render(response, "list.html", {"ls":ls})
+
+def home(response):
+    return render(response, "home.html", {})
+
+def create(response):
+    if response.method == "POST":
+        form = CreateNewList(response.POST)
+        if form.is_valid():
+            n = form.cleaned_data['name']
+            t = TDL(name=n)
+            t.save()
+        return HttpResponseRedirect("/%i" % t.id)
+    else:
+        form = CreateNewList()
+    return render(response, "create.html", {"form":form})
